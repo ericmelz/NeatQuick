716d29eb4c7ba40cd8daa13307c20cf80ea2650d
Eric Melz <eric@emelz.com>
2011-03-20 22:43:33 -0700

    README tweaks

--- src/main/groovy/com/emelz/transfer/Launcher.java
+++ src/main/groovy/com/emelz/transfer/Launcher.java
@@ -11,7 +11,6 @@ import java.net.URLClassLoader;
 class Launcher {
     public static final String NEAT_DATA_DIR = "/Users/eric/Documents/Neat Library.nrmlib";
     public static final String QUICKEN_DATA_DIR = "/Users/eric/Documents/Quicken Experiments/Quicken One Transaction.quickendata";
-    //public static final String QUICKEN_DATA_DIR = "/Users/eric/Documents/Quicken Experiments/Quicken Baseline.quickendata";
     public static void main(String[] args) throws Exception {
         TransferUtil tu = new TransferUtil(NEAT_DATA_DIR, QUICKEN_DATA_DIR);
         tu.doTransfer(null);
